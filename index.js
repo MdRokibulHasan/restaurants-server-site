67911bbf87e5af7d23ab13aeec2f1caf11460b72
MdRokibulHasan <mdrokibulhasan42@gmail.com>
2022-04-08 04:09:55 +0600

    added delete api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const ObjectId = require("mongodb").ObjectId;
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 
 // moddelewaare
@@ -16,12 +18,35 @@ async function run() {
     await client.connect();
     const database = client.db("online_restaurants");
     const productCollection = database.collection("products");
+    const ordersCollectioin = database.collection("orders");
 
     // GET Products API
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
       const products = await cursor.toArray();
       res.send(products);
+    });
+
+    // POST order API
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollectioin.insertOne(order);
+      res.json(result);
+    });
+
+    app.get("/order", async (req, res) => {
+      const cursor = ordersCollectioin.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    // DELETE API
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollectioin.deleteOne(query);
+      res.json(result);
     });
   } finally {
     // await client.close();
