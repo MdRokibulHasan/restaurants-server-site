dfb0eea744721b0aa4a1cd5c7d3ea99763b34949
MdRokibulHasan <mdrokibulhasan42@gmail.com>
2022-04-07 23:24:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// moddelewaare
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9lpvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("online_restaurants");
+    const productCollection = database.collection("products");
+
+    // GET Products API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Resturent server is running");
+});
+
+app.listen(port, () => {
+  console.log("Srver is running at port 5000");
+});
